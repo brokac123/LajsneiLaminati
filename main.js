e0faf3c58c5c4936f9117e7d3a20e7adfd5f8a3a
brokac123 <luka.bakula1@gmail.com>
2023-01-25 16:36:22 +0100

    new JS feature

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const egger = document.querySelector('#egger');
 const krono = document.querySelector('#krono');
 const binyl = document.querySelector('#binyl');
+const showAll=document.querySelector('#all')
 
 const eggerSort = document.querySelectorAll('.egger')
 const kronoSort = document.querySelectorAll('.krono');
 const binylSort = document.querySelectorAll('.biynl')
+
 
 egger.addEventListener('click', () => {
     for (let i = 0; i < eggerSort.length; i++) {
@@ -39,3 +41,16 @@ binyl.addEventListener('click', () => {
         eggerSort[i].classList.add('none');
     }
 })
+showAll.addEventListener('click',()=>{
+    for (let i = 0; i < binylSort.length; i++) {
+        binylSort[i].classList.remove('none');
+    }
+    for (let i = 0; i < kronoSort.length; i++) {
+        kronoSort[i].classList.remove('none');
+    }
+    for (let i = 0; i < eggerSort.length; i++) {
+        eggerSort[i].classList.remove('none');
+    }
+
+}
+)
